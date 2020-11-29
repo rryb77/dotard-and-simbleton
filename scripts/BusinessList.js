0e65173b0a10602c44a7805dea43f03ce3abb5d5
rryb77 <rryb77@gmail.com>
2020-11-29 10:12:13 -0600

    Added search functionality to the site. Can search by company name

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -17,3 +17,46 @@ export const businessList = () => {
     );
 
 }
+
+const companySearchResultArticle = document.querySelector(".foundCompanies")
+
+document
+    .querySelector("#companySearch")
+    .addEventListener("keypress", keyPressEvent => {
+        if (keyPressEvent.charCode === 13) {
+            /*
+                When user presses enter, find the matching business.
+                You can use the `.includes()` method strings to
+                see if a smaller string is part of a larger string.
+
+                Example:
+                    business.companyName.includes(keyPressEvent.target.value)
+            */
+            
+            // implement .find() method here
+            // const foundBusiness = businessArray.find((business) => {
+            //     let userInput = document.getElementById("companySearch").value;
+            //     console.log('The user typed: ', userInput)
+
+            //     return business.companyName === userInput
+            // })
+            
+            const foundBusiness = businessArray.find(business => business.companyName.includes(document.getElementById("companySearch").value))
+
+
+            companySearchResultArticle.innerHTML = `
+                <h2>
+                ${foundBusiness.companyName}
+                </h2>
+                <section>
+                ${foundBusiness.addressFullStreet}
+
+                </section>
+                <section>
+                ${foundBusiness.addressCity},
+                ${foundBusiness.addressStateCode}
+                ${foundBusiness.addressZipCode}
+                </section>
+            `;
+        }
+    });
